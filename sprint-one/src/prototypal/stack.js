040eb2ae308a758b9fa76533f6cfc965033b0410
Hack Reactor Students <accounts@hackreactor.com>
2015-02-04 16:40:27 -0800

    Write prototypical

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,7 +1,25 @@
 var Stack = function() {
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
+  newStack.length = 0;
 
+  return newStack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(val){
+    this.storage[this.length] = val;
+    this.length++;
+  },
+  pop: function(){
+    var result = this.storage[this.length - 1];
+    delete this.storage[this.length -1];
+    this.length && this.length--;
+    return result;
+  },
+  size: function(){
+    return this.length;
+  }
+};
 
 
