109d0329241a5df628a50fe6af7e12d2c0ec3924
Hack Reactor Students <accounts@hackreactor.com>
2015-02-04 22:56:16 -0800

    Began writing linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,25 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = Node(value);
+    this.tail ? this.tail.next = node : this.head = node;
+    this.tail = node;
   };
 
   list.removeHead = function(){
+    var currentHead = this.head;
+    var result = currentHead.value;
+    this.head = this.head.next;
+    // is currentHead going to garbage collect?
+    currentHead.next = null;
+    currentHead.value = null;
+    currentHead = null;
+    //currentHead.explode(); === null all the things
+    return result;
   };
 
   list.contains = function(target){
+
   };
 
   return list;
