3eaa5014d6337624843dd092efdc197b394f07f0
Hack Reactor Students <accounts@hackreactor.com>
2015-02-07 11:51:13 -0800

    Re-implemented hash table after data loss

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,38 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if ( !this._storage.get(i) ) {
+    // create a new bucket
+    this._storage.set(i, []);
+  }
+  this._storage.get(i).push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var bucket = this._storage.get(getIndexBelowMaxForKey(k, this._limit));
+  var result;
+  if (bucket) {
+    for (var i=0, l=bucket.length; i<l; i++){
+      if (bucket[i][0] === k) {
+        return bucket[i][1];
+      }
+    }
+  }
+  // accomodating a customer who for some reason wants 'null'
+  // where any reasonable person would want 'undefined'
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
-
+  var bucket = this._storage.get(getIndexBelowMaxForKey(k, this._limit));
+  var result;
+  if (bucket) {
+    for (var i=0, l=bucket.length; i<l; i++){
+      if (bucket[i][0] === k) {
+        bucket.splice(i, 1);
+      }
+    }
+  }
 };
 
 
